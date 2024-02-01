720b7ebc5167a260ef3219a29dd325ddf1228faa
daragh coll <daraghcoll@daraghs-MacBook-Air.local>
2024-02-02 05:38:39 +1100

    feat: update wave animation rotation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,9 @@ export default {
 				wave: {
 					'0%': { transform: 'rotate(0.0deg)' },
 					'10%': { transform: 'rotate(20deg)' },
-					'20%': { transform: 'rotate(-8deg)' },
+					'20%': { transform: 'rotate(-20deg)' },
 					'30%': { transform: 'rotate(20deg)' },
-					'40%': { transform: 'rotate(-4deg)' },
+					'40%': { transform: 'rotate(-8deg)' },
 					'50%': { transform: 'rotate(10.0deg)' },
 					'60%': { transform: 'rotate(0.0deg)' },
 					'100%': { transform: 'rotate(0.0deg)' }
