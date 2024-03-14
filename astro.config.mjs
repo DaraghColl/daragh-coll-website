3a32502395e9ad2f775d4ab2d6a0416d6c1613b7
daragh coll <daraghcoll@daraghs-Air.station>
2024-03-14 00:25:31 +0000

    feat: package for code blocks in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import mdx from '@astrojs/mdx';
+
+import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx()]
+	integrations: [
+		tailwind(),
+		expressiveCode({
+			frames: {
+				showCopyToClipboardButton: true
+			},
+			styleOverrides: {
+				// You can optionally override the plugin's default styles here
+				frames: {
+					editorActiveTabIndicatorTopColor: 'transparent',
+					editorActiveTabIndicatorBottomColor: '#4f46e5'
+				}
+			}
+		}),
+		mdx()
+	]
 });
